31609b441ff5c9dcb5bf9e25dde800b0e819d8ac
Gaeltomaz123 <gaeltomaz@gmail.com>
2023-11-19 19:53:03 -0300

    Implementando a classe criar trem

--- TFPOO/src/main/java/com/teste/Main.java
+++ TFPOO/src/main/java/com/teste/Main.java
@@ -1,7 +1,9 @@
 package com.teste;
 
+import com.teste.View.TransicaoTelas;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new TransicaoTelas();
     }
 }
--- TFPOO/src/main/java/com/teste/View/CriarTrem.java
+++ TFPOO/src/main/java/com/teste/View/CriarTrem.java
@@ -1,10 +1,32 @@
 package com.teste.View;
 
 import javax.swing.*;
+
+import com.teste.Model.Carro;
+import com.teste.Model.Composicao;
+import com.teste.Model.Locomotiva;
+import com.teste.Model.Vagao;
+
 import java.awt.*;
+import java.util.ArrayList;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class CriarTrem extends JPanel {
+    private ArrayList<Locomotiva> arrayLocomotivas = new ArrayList<Locomotiva>();
+    private ArrayList<Vagao> arrayVagao = new ArrayList<Vagao>();
+    private ArrayList<Composicao> arrayComposicao = new ArrayList<Composicao>();
+
+    public void condicoesIniciais() {
+        for (int i = 0; i < 5; i++) {
+            arrayVagao.add(new Vagao(i, 200, null));
+        }
+        for (int i = 0; i < 3; i++) {
+            arrayLocomotivas.add(new Locomotiva(i, 1000, 10, null));
+        }
+    }
     public CriarTrem() {
+        condicoesIniciais();
         setBackground(new Color(63, 55, 55));
         setPreferredSize(new Dimension(400, 575));
         setLayout(new GridBagLayout());
@@ -47,6 +69,26 @@ public class CriarTrem extends JPanel {
         button1.setFont(font);
         button1.setForeground(Color.WHITE);
         button1.setBackground(new Color(99, 98, 98));
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO Auto-generated method stub
+                
+                
+                //Pop up
+                String id = textFieldID.getText();
+                if (id.matches("[0-9]+")) {
+                    JOptionPane.showMessageDialog(labelID, "SUCESSO! TREM CRIADO - ID: " + id);
+                    ArrayList<Carro> carro = new ArrayList<Carro>();
+                    carro.add(arrayLocomotivas.get(0));
+                    arrayComposicao.add(new Composicao(Integer.parseInt(id), carro));
+                    arrayLocomotivas.remove(0);
+                } else {
+                    JOptionPane.showMessageDialog(labelID, "ERRO: Digite apenas números");
+                }
+
+            }
+        });
         gbc.gridy++;
         add(button1, gbc);
     }
