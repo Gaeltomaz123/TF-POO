2ad5509deff4b7f49aff1c77947b4c0542952137
Gaeltomaz123 <gaeltomaz@gmail.com>
2023-11-23 22:57:03 -0300

    adicionando os metodos para guardar no csv

--- TFPOO/src/main/java/com/teste/View/ListarTrem.java
+++ TFPOO/src/main/java/com/teste/View/ListarTrem.java
@@ -3,14 +3,24 @@ package com.teste.View;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 
+import com.teste.Model.Carro;
 import com.teste.Model.Composicao;
 import com.teste.Model.Locomotiva;
 import com.teste.Model.Vagao;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.stream.Stream;
 
 public class ListarTrem extends JPanel {
     private int trensDeletados = 0; // Variável para contar os trens deletados
@@ -22,6 +32,12 @@ public class ListarTrem extends JPanel {
     JPanel contentPanel = new JPanel(new GridBagLayout());
 
     public ListarTrem() {
+        String locomotivas = "TFPOO/src/main/java/com/teste/Model/Locomotivas.csv"; 
+        String vagoes = "TFPOO/src/main/java/com/teste/Model/Vagoes.csv";  
+
+        lerLinhas(locomotivas);
+        lerLinhas(vagoes);
+        lerComposicoes();
 
         setLayout(new BorderLayout());
         //JPanel contentPanel = new JPanel(new GridBagLayout());
@@ -37,6 +53,8 @@ public class ListarTrem extends JPanel {
             addLine(contentPanel, gbc, i);
         }
 
+        addButtonSave(contentPanel, gbc);
+
         JScrollPane scrollPane = new JScrollPane(contentPanel); // JScrollPane para o conteúdo
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER); // Desabilita a barra
@@ -44,6 +62,185 @@ public class ListarTrem extends JPanel {
 
         add(scrollPane, BorderLayout.CENTER); // Adiciona o JScrollPane ao painel principal
     }
+
+    private void addButtonSave(JPanel contentPanel, GridBagConstraints gbc) {
+        JPanel innerPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        innerPanel.setPreferredSize(new Dimension(150, 30));
+        innerPanel.setMaximumSize(new Dimension(150, 25));
+        innerPanel.setMinimumSize(new Dimension(150, 25));
+        innerPanel.setBackground(new Color(38, 105, 21));
+
+        JButton labelsave = new JButton("Salvar");
+        labelsave.setForeground(Color.WHITE);
+        labelsave.setBackground(new Color(38, 105, 21));
+        labelsave.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        labelsave.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                labelsave.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+                String locomotivas = "TFPOO/src/main/java/com/teste/Model/Locomotivas.csv";
+                String vagoes = "TFPOO/src/main/java/com/teste/Model/Vagoes.csv";
+                String composicoes = "TFPOO/src/main/java/com/teste/Model/Composicoes.csv";
+
+                apagarLinhas(locomotivas);
+                apagarLinhas(vagoes);
+                apagarLinhas(composicoes);
+
+                escreverLinhas(locomotivas);
+                escreverLinhas(vagoes);
+                escreverComposicao();
+            }
+    });
+        innerPanel.add(labelsave);
+        contentPanel.add(innerPanel, gbc);
+        gbc.gridy++; 
+    }
+
+    public void escreverComposicao() {
+        String composicoes = "TFPOO/src/main/java/com/teste/Model/Composicoes.csv";
+        //try { 
+                
+                // Abre o escritor para adicionar dados ao arquivo
+                //BufferedWriter escritor = new BufferedWriter(new FileWriter(composicoes));
+                /*if(caminho == "TFPOO/src/main/java/com/teste/Model/Locomotivas.csv") {
+                    for(int i=0; i<arrayLocomotivas.size(); i++) {
+                    escritor.write("L" + i);
+                    escritor.newLine();
+                }
+                
+                }
+                if(caminho == "TFPOO/src/main/java/com/teste/Model/Vagoes.csv") {
+                    for(int i=0; i<arrayVagao.size(); i++) {
+                    escritor.write("V" + i);
+                    escritor.newLine();
+                }
+                }
+                escritor.flush();
+                escritor.close();
+                } catch (IOException a) {
+                    a.printStackTrace();
+                } */
+    }
+
+    public void lerComposicoes() {
+        String composicoes = "TFPOO/src/main/java/com/teste/Model/Composicoes.csv";
+        BufferedReader reader = null;
+        String line = "";
+
+        try {
+            reader = new BufferedReader(new FileReader(composicoes));
+            int count = 0;
+            while((line = reader.readLine()) != null) {
+                ArrayList<Carro> carro = new ArrayList<Carro>();
+                Composicao composicao = new Composicao(count, carro);
+                for(int i=0; i<line.length(); i++){
+                    int countL = 0;
+                    int countV = 0;
+                    if(line.charAt(i) == 'L') {
+                        carro.add(arrayLocomotivas.get(countL));
+                        countL ++;
+                    }
+                    if(line.charAt(i) == 'V') {
+                        carro.add(arrayVagao.get(countV));
+                        countV ++;
+                    }
+                }
+                count++;
+                arrayComposicao.add(composicao);
+            }
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void lerLinhas(String caminho) {
+        BufferedReader reader = null;
+        String line = "";
+
+        try {
+            reader = new BufferedReader(new FileReader(caminho));
+            while((line = reader.readLine()) != null) {
+                if(caminho == "TFPOO/src/main/java/com/teste/Model/Locomotivas.csv") {
+                    for(int i=0; i<line.lines().count(); i++) {
+                        arrayLocomotivas.add(new Locomotiva(i, 1000, 10, null));
+                    }
+                }
+                if(caminho == "TFPOO/src/main/java/com/teste/Model/Vagoes.csv") {
+                    for(int i=0; i<line.lines().count(); i++) {
+                        arrayVagao.add(new Vagao(i, 200, null));
+                    }
+                }
+            }
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void escreverLinhas(String caminho) {
+        try { 
+                // Abre o escritor para adicionar dados ao arquivo
+                BufferedWriter escritor = new BufferedWriter(new FileWriter(caminho));
+                if(caminho == "TFPOO/src/main/java/com/teste/Model/Locomotivas.csv") {
+                    for(int i=0; i<arrayLocomotivas.size(); i++) {
+                    escritor.write("L" + i);
+                    escritor.newLine();
+                }
+                
+                }
+                if(caminho == "TFPOO/src/main/java/com/teste/Model/Vagoes.csv") {
+                    for(int i=0; i<arrayVagao.size(); i++) {
+                    escritor.write("V" + i);
+                    escritor.newLine();
+                }
+                }
+                escritor.flush();
+                escritor.close();
+                } catch (IOException a) {
+                    a.printStackTrace();
+                }  
+    }
+
+    public void apagarLinhas(String caminho) {
+        BufferedReader reader = null;
+        BufferedWriter escritor = null;
+        String line = "";
+
+        try {
+            reader = new BufferedReader(new FileReader(caminho));
+            escritor = new BufferedWriter(new FileWriter(caminho));
+            while((line = reader.readLine()) != null) {
+                escritor.write("");
+                escritor.newLine();
+            } 
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     public void addLine() {
         addLine(contentPanel, gbc, arrayComposicao.size()-1);
     }
